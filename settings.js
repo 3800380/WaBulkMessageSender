bcd02decca95119be613e816375f64f14db20913
3800380 <talkdrov@gmail.com>
2024-09-21 06:35:44 -0700

    Update settings.js

---
+++ settings.js
@@ -0,0 +1,6 @@
+const config = {
+    NUMBER: '923072380380',
+    SESSION_ID: '' //Abhi work nahi kary ga, abhi direct session folder mein creds.json mein session id dycrypt ker ke paste karo......
+};
+
+module.exports = config;
